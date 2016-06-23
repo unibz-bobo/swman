aad0bbe6e4ee2921638ed90bdc276723416aa1fb
Julian Sanin <sanin89julian@gmail.com>
2016-06-23 16:48:08 +0200

    Added service abstraction

--- swman.lib.sh
+++ swman.lib.sh
@@ -1,5 +1,83 @@
 #!/bin/bash
 
+# Init system. Sets the variable INIT_LIKE containing the most generic
+# init system.
+# Example: INIT_LIKE=systemd, or ID_LIKE=sysvinit
+detectInitSystem() {
+  which systemd > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    INIT_LIKE=systemd
+    return
+  fi
+  which service > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    INIT_LIKE=sysvinit
+    return
+  fi
+  INIT_LIKE=""
+}
+
+# DO NOT CHANGE! This is needed to detect the init system.
+detectInitSystem
+
+# Start a service.
+serviceStart() {
+  local serviceName=$1
+  if [ $INIT_LIKE = systemd ]; then
+    systemctl enable $serviceName
+    systemctl start $serviceName
+    return $?
+  fi
+  if [ $INIT_LIKE = sysvinit ]; then
+    service $serviceName start
+    return $?
+  fi
+  return 0
+}
+
+# Stop a service.
+serviceStop() {
+  local serviceName=$1
+  if [ $INIT_LIKE = systemd ]; then
+    systemctl disable $serviceName
+    systemctl stop $serviceName
+    return $?
+  fi
+  if [ $INIT_LIKE = sysvinit ]; then
+    service $serviceName stop
+    return $?
+  fi
+  return 0
+}
+
+# Restart a service.
+serviceRestart() {
+  local serviceName=$1
+  if [ $INIT_LIKE = systemd ]; then
+    systemctl restart $serviceName
+    return $?
+  fi
+  if [ $INIT_LIKE = sysvinit ]; then
+    service $serviceName restart
+    return $?
+  fi
+  return 0
+}
+
+# Query the status of a service.
+serviceStatus() {
+  local serviceName=$1
+  if [ $INIT_LIKE = systemd ]; then
+    systemctl status $serviceName
+    return $?
+  fi
+  if [ $INIT_LIKE = sysvinit ]; then
+    service $serviceName status
+    return $?
+  fi
+  return 0
+}
+
 # Sets the variable ID_LIKE containing the most generic
 # distribution name.
 # Example: ID_LIKE=arch, or ID_LIKE=debian
@@ -22,7 +100,6 @@ detectPacbobo() {
 
 # DO NOT CHANGE! This is needed to detect the pacbobo package manager.
 detectPacbobo
-
 
 # Query if a package is already installed.
 packageQuery() {
